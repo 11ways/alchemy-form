ccbbbef9b093dff8ccd43a02f12fe48fe5f3fff5
Jelle De Loecker <jelle@elevenways.be>
2020-12-21 16:17:35 +0100

    Add alchemy-toggle & alchemy-code element

---
+++ element/alchemy_toggle.js
@@ -0,0 +1,75 @@
+/**
+ * The alchemy-toggle custom element
+ *
+ * @author   Jelle De Loecker   <jelle@elevenways.be>
+ * @since    0.1.0
+ * @version  0.1.0
+ */
+const AlchemyToggle = Function.inherits('Alchemy.Element.Form.Base', 'Toggle');
+
+/**
+ * The template to use for the content of this element
+ *
+ * @author   Jelle De Loecker   <jelle@elevenways.be>
+ * @since    0.1.0
+ * @version  0.1.0
+ */
+AlchemyToggle.setTemplateFile('form/elements/alchemy_toggle');
+
+/**
+ * The stylesheet to load for this element
+ *
+ * @author   Jelle De Loecker <jelle@elevenways.be>
+ * @since    0.1.0
+ * @version  0.1.0
+ */
+AlchemyToggle.setStylesheetFile('form/alchemy_toggle');
+
+/**
+ * Get/set the value
+ *
+ * @author   Jelle De Loecker <jelle@elevenways.be>
+ * @since    0.1.0
+ * @version  0.1.0
+ */
+AlchemyToggle.setAttribute('value', null, function setValue(value) {
+
+	let input = this.querySelector('input');
+
+	value = !!value;
+
+	if (input) {
+		input.checked = value;
+	}
+
+	return value;
+
+}, {boolean: true});
+
+/**
+ * Added to the dom
+ *
+ * @author   Jelle De Loecker <jelle@elevenways.be>
+ * @since    0.1.0
+ * @version  0.1.0
+ */
+AlchemyToggle.setMethod(function introduced() {
+
+	this.addEventListener('click', e => {
+		this.value = !this.value;
+	});
+
+	let input = this.querySelector('input');
+
+	input.addEventListener('change', e => {
+
+		e.preventDefault();
+		e.stopPropagation();
+
+		if (input.checked == this.value) {
+			return;
+		}
+
+		this.value = input.checked;
+	});
+});
---
+++ element/code_input.js
@@ -0,0 +1,67 @@
+/**
+ * The code-input custom element
+ *
+ * @author   Jelle De Loecker   <jelle@elevenways.be>
+ * @since    0.1.0
+ * @version  0.1.0
+ */
+const CodeInput = Function.inherits('Alchemy.Element.Form.Base', 'CodeInput');
+
+/**
+ * The template to use for the content of this element
+ *
+ * @author   Jelle De Loecker   <jelle@elevenways.be>
+ * @since    0.1.0
+ * @version  0.1.0
+ */
+CodeInput.setTemplateFile('form/elements/code_input');
+
+/**
+ * The stylesheet to load for this element
+ *
+ * @author   Jelle De Loecker <jelle@elevenways.be>
+ * @since    0.1.0
+ * @version  0.1.0
+ */
+CodeInput.setStylesheetFile('form/alchemy_code_input');
+
+/**
+ * Get/set the value
+ *
+ * @author   Jelle De Loecker <jelle@elevenways.be>
+ * @since    0.1.0
+ * @version  0.1.0
+ */
+CodeInput.setProperty(function value(value) {
+
+	if (this._editor) {
+		return this._editor.getValue();
+	}
+
+	let editor_el = this.querySelector('.code-editor');
+
+	if (editor_el) {
+		return editor_el.textContent;
+	}
+});
+
+/**
+ * Added to the dom
+ *
+ * @author   Jelle De Loecker <jelle@elevenways.be>
+ * @since    0.1.0
+ * @version  0.1.0
+ */
+CodeInput.setMethod(async function introduced() {
+
+	await hawkejs.require('ace1.4/ace');
+
+	let editor_el = this.querySelector('.code-editor');
+
+	let editor = ace.edit(editor_el);
+
+	this._editor = editor;
+
+	console.log(editor_el, 'EDITOR:', editor);
+
+});
