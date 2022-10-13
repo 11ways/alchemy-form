7b1ab3e047805ce8143ff471bc409ab1ef6dca55
Jelle De Loecker <jelle@elevenways.be>
2022-10-13 13:19:55 +0200

    Rename all element javascript files

---
+++ element/al_state.js
@@ -0,0 +1,8 @@
+/**
+ * The al-state element
+ *
+ * @author   Jelle De Loecker   <jelle@elevenways.be>
+ * @since    0.2.0
+ * @version  0.2.0
+ */
+const State = Function.inherits('Alchemy.Element.Form.Stateful', 'State');
